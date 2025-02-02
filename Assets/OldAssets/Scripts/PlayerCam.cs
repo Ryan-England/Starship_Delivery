64666db4de40a4d5dae19d04ca86b503993a0341
ali <alitaquie51@gmail.com>
2025-02-02 15:21:09 -0800

    finalized camera movements

--- Assets/OldAssets/Scripts/PlayerCam.cs
+++ Assets/OldAssets/Scripts/PlayerCam.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class PlayerCam : MonoBehaviour
 {
-
     public Transform orientation;
+    public float arrowKeyRotationSpeed = 100f;
 
     float xRotation;
     float yRotation;
@@ -16,15 +16,27 @@ public class PlayerCam : MonoBehaviour
 
     private void Update()
     {
-        // get mouse input
+
         float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * (PauseMenu.mouseValue * 50f);
         float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * (PauseMenu.mouseValue * 50f);
 
-        yRotation += mouseX;
-        xRotation -= mouseY;
+        float arrowHorizontal = 0f;
+        float arrowVertical = 0f;
+
+        if (Input.GetKey(KeyCode.RightArrow)) arrowHorizontal += 1f;
+        if (Input.GetKey(KeyCode.LeftArrow)) arrowHorizontal -= 1f;
+        if (Input.GetKey(KeyCode.DownArrow)) arrowVertical -= 1f;
+        if (Input.GetKey(KeyCode.UpArrow)) arrowVertical += 1f;
+
+        
+        arrowHorizontal *= Time.deltaTime * arrowKeyRotationSpeed;
+        arrowVertical *= Time.deltaTime * arrowKeyRotationSpeed;
+
+       
+        yRotation += mouseX + arrowHorizontal;
+        xRotation -= mouseY + arrowVertical;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        //rotate cam and orientaton
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
         orientation.rotation = Quaternion.Euler(0, yRotation, 0);
     }
