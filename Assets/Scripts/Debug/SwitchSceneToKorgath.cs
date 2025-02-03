ebe8ed139cabf1acd69e16481afb5acc9eb31ec4
Quinn-Hub <56490493+Quinn-Hub@users.noreply.github.com>
2025-02-03 00:47:43 -0800

    Update: Fixed Camera Bug
    ==========
     Changelog
    ==========
    - Was missing the UI group for Jacky's pause menu script inside MoveTestScene. This caused the camera to be frozen in place.
    - Added a Debug folder with a script to change to the experimental Korgath landscape scene to test it out.
    - Added both the MoveTestScene and the Korgath scene to buildSettings so that you can load the scenes via script and when building the game, the scenes are accessible.

---
+++ Assets/Scripts/Debug/SwitchSceneToKorgath.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchSceneToKorgath : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            SceneManager.LoadScene("Korgath");
+        }
+    }
+}
