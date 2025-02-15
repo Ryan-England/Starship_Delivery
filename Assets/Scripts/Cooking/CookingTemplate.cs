1915ba575a28203142b6b3cbac3e7b3fa6729acd
ali <alitaquie51@gmail.com>
2025-02-15 14:41:33 -0800

    implemented cooking system interface

--- Assets/Scripts/Cooking/CookingTemplate.cs
+++ Assets/Scripts/Cooking/CookingTemplate.cs
@@ -1,34 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;  // Required for UI elements
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class CookingTemplate : MonoBehaviour
 {
     public PlayerMovement pm; 
     public GameObject kitchenUI;
-    public GameObject start;
+    public GameObject startButton;
+    public Text timerText;
+    public float cookingTime = 5f;
+    private bool isCooking = false;
+
     List<GameObject> cook_it = new List<GameObject>();
     public Transform cook_items;
-    public Fridge f; 
+    public Fridge f;
 
     Dictionary<string, int> csub = new Dictionary<string, int>();
 
-    // Start is called before the first frame update
     void Start()
     {
-        foreach(Transform k in cook_items){
+        foreach (Transform k in cook_items)
+        {
             cook_it.Add(k.gameObject);
-        }   
+        }
+        startButton.GetComponent<Button>().interactable = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        CheckSlots();
     }
-    public void KitchenMenu(){
+
+    public void KitchenMenu()
+    {
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
         pm.enabled = false;
@@ -36,91 +42,106 @@ public class CookingTemplate : MonoBehaviour
         kitchenUI.SetActive(true);
     }
 
-    //basically we want to check if the items have a 
-    public void BackButtonKitchen(){
+    public void BackButtonKitchen()
+    {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         Time.timeScale = 1;
-        pm.enabled =true;
+        pm.enabled = true;
         kitchenUI.SetActive(false);
     }
 
-    public void MoveToCook(){
-        //go inside the canvas inventory next to the fridge
+    public void MoveToCook()
+    {
         GameObject temp = EventSystem.current.currentSelectedGameObject.transform.parent.gameObject;
-        // GameObject temp = gameObject.transform.parent.gameObject;
-        Slot s  = temp.GetComponent<Slot>();
-        if(s.filled && s.action == "chopping"){
+        Slot s = temp.GetComponent<Slot>();
+
+        if (s.filled && s.action == "chopping")
+        {
             AddToCook(s.name, 1);
         }
     }
 
-    public void AddToCook(string name, int quantity){
-        foreach(GameObject j in cook_it){
-             GameObject temp = j.transform.Find("Items").Find(name).gameObject;
-             GameObject qty = j.transform.Find("qty").gameObject; 
-             Text t = qty.GetComponent<Text>();
-             Slot s = j.GetComponent<Slot>();
+    public void AddToCook(string name, int quantity)
+    {
+        foreach (GameObject j in cook_it)
+        {
+            GameObject temp = j.transform.Find("Items").Find(name).gameObject;
+            GameObject qty = j.transform.Find("qty").gameObject;
+            Text t = qty.GetComponent<Text>();
+            Slot s = j.GetComponent<Slot>();
 
-             if(!s.filled){
-                //check the name of the item
-                //check the quantity
-                //set active the icon in that slot 
-                //update the qty according to the item
-                switch (name){
-                    case "apple":
-                    case "banana":
-                    case "Apple": 
-                    case "Banana":
-                        temp.SetActive(true);
+            if (!s.filled)
+            {
+                temp.SetActive(true);
+                t.text = "x" + quantity;
+                s.filled = true;
+                s.name = name;
 
-                        t.text= "x" + quantity;
-
-                        s.filled = true;
-
-                        s.name = name;
-                        if(!csub.ContainsKey(name)){
-                            csub.Add(name, quantity);
-                        }
-                        else{
-                            csub[name] +=1; 
-                        }
-                        
-                        break;
-                    default:
-                        Debug.Log("sdf;lks;dlfk;sldkf");
-                        break;
+                if (!csub.ContainsKey(name))
+                {
+                    csub.Add(name, quantity);
                 }
+                else
+                {
+                    csub[name] += 1;
+                }
+
                 f.DeleteItems(name, 1);
                 return;
             }
-            // else if(s.filled && s.name == name){
-            //     Debug.Log("test1");
-            //     //check the name of the item
-            //     //check the quantity
-            //     //set active the icon in that slot 
-            //     //update the qty according to the item
-            //     switch (name){
-            //         case "apple":
-            //         case "banana":
-            //         case "Apple": 
-            //         case "Banana":
-            //             temp.SetActive(true);
-
-            //             if(csub[name]+1 <= f.fsub[name]){
-            //                 t.text= "x" + (csub[name]+1);
-            //                 csub[name] +=1;
-            //             }
-            //             else{
-            //                 t.text= "x" + (csub[name]);
-            //             }
-            //             break;
-            //         default:
-            //             Debug.Log("sdf;lks;dlfk;sldkf");
-            //             break;
-            //     }
-
-            // }
         }
+    }
+
+    private void CheckSlots()
+    {
+        int filledCount = 0;
+        foreach (GameObject slotObj in cook_it)
+        {
+            Slot slot = slotObj.GetComponent<Slot>();
+            if (slot.filled) filledCount++;
+        }
+        startButton.GetComponent<Button>().interactable = (filledCount >= 2 && !isCooking);
+    }
+
+    public void StartCooking()
+    {
+        if (!isCooking)
+        {
+            StartCoroutine(CookingProcess());
+        }
+    }
+
+    private IEnumerator CookingProcess()
+    {
+        isCooking = true;
+        float timer = cookingTime;
+
+        foreach (GameObject slotObj in cook_it)
+        {
+            Slot slot = slotObj.GetComponent<Slot>();
+            if (slot.filled)
+            {
+                slot.filled = false;
+                slot.name = "";
+                Transform itemContainer = slotObj.transform.Find("Items");
+                foreach (Transform item in itemContainer)
+                {
+                    item.gameObject.SetActive(false);
+                }
+                Text qtyText = slotObj.transform.Find("qty").GetComponent<Text>();
+                qtyText.text = "";
+            }
+        }
+
+        while (timer > 0)
+        {
+            timerText.text = "Cooking... " + Mathf.Ceil(timer) + "s";
+            yield return new WaitForSeconds(1f);
+            timer--;
+        }
+
+        timerText.text = "Done Cooking!";
+        isCooking = false;
     }
 }
