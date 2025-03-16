cc84fefb4ba2becf76af613aa6312f6b9da13e0e
Carolyn-Hope256 <cmhope@ucsc.edu>
2025-03-16 08:00:13 -0700

    implemented lactite and added several item sprites. Reworked the recipe dictionary to be more organized, and now contains a list of every implemented and unimplemented item for the vertical slice.

--- Assets/Scripts/Dictionaries/RecipeDictionary.cs
+++ Assets/Scripts/Dictionaries/RecipeDictionary.cs
@@ -13,10 +13,12 @@ public class RecipeDictionary : MonoBehaviour
     void Start()
     {
         LoadItemDict();
-        BakeDict.Add("smolderdough", new recipe("quaso", 1));
-        MixDict.Add("applebanana", new recipe("ice", 1));
-        MixDict.Add("cinderwheatsalt", new recipe("smolderdough", 1));
-        MixDict.Add("glegle", new recipe("salad", 1));
+        LoadMixDict();
+        LoadBakeDict();
+        
+        
+
+        
     }
 
     // Update is called once per frame
@@ -42,18 +44,74 @@ public class RecipeDictionary : MonoBehaviour
 
     private void LoadItemDict()
     {
+        //placeholder ingredients
         Items.Add("apple", "ingredient");
         Items.Add("banana", "ingredient");
+        Items.Add("glegle", "ingredient");
+
+        //Implemented ingredients
         Items.Add("cinderwheat", "ingredient");
         Items.Add("salt", "ingredient");
+        Items.Add("lactite", "ingredient");
         Items.Add("smolderdough", "ingredient");
-        Items.Add("glegle", "ingredient");
-        Items.Add("quaso", "food");
+        Items.Add("silkydough", "ingredient");
+        Items.Add("krocream", "ingredient");
+
+        //Ingredients needing sprites
+        Items.Add("mohofruit", "ingredient"); //TL note: the moho is the boundary between the crust and the mantle
+        Items.Add("mohokrocream", "ingredient"); //could be used as a food item as well
+        Items.Add("mohojam", "ingredient");
+        Items.Add("mohofilling", "ingredient");
+
+        //placeholder foods
         Items.Add("ice", "food");
         Items.Add("salad", "food");
 
+        //Implemented foods
+        Items.Add("cinderloaf", "food");
+        Items.Add("quaso", "food");
+
+        //Foods needing sprites
+        Items.Add("mohotart", "food");
+        Items.Add("krocreambun", "food");
+        Items.Add("mohopie", "food");
+    }
+
+    private void LoadMixDict()
+    {
+        //placeholder recipes
+        MixDict.Add("applebanana", new recipe("ice", 1));
+        MixDict.Add("glegle", new recipe("salad", 1));
+
+        //Implemented recipes
+        MixDict.Add("cinderwheatsalt", new recipe("smolderdough", 1));
+        MixDict.Add("lactitesmolderdough", new recipe("silkydough", 1));
+        MixDict.Add("lactitesalt", new recipe("krocream", 1));
+
+        //Recipes needing sprites
+        MixDict.Add("mohofruitsalt", new recipe("mohojam", 1));
+        MixDict.Add("mohofruitmohojam", new recipe("mohofilling", 1));
+        MixDict.Add("krocreammohofruit", new recipe("mohokrocream", 1));
+
+    }
+
+    private void LoadBakeDict()
+    {
+       
+        //Implemented recipes
+        BakeDict.Add("smolderdough", new recipe("cinderloaf", 1));
+        BakeDict.Add("silkydough", new recipe("quaso", 1));
+
+        //Recipes needing sprites
+        
+        BakeDict.Add("krocreamsilkydough", new recipe("krocreambun", 1));
+        BakeDict.Add("mohojamsmolderdough", new recipe("mohotart", 1));
+        BakeDict.Add("mohofillingsilkydough", new recipe("mohopie", 1));
+
     }
 }
+
+
 
 public struct recipe
 {
